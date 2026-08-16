e0a78776dda8ddc751e7d475ef79c26d378705e6
JordanSearle <jordansearle@msn.com>
2026-08-16 11:12:01 +0100

    init boostrap and react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Import our custom CSS
+import './scss/styles.scss'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
